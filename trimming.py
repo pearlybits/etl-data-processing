347251bfa98c4080a328de568cb9c499a6e38a59
pawannegiBits <2020mt93114@wilp.bits-pilani.ac.in>
2021-04-15 22:39:24 +0530

    trimming functionality added #1

---
+++ trimming.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+
+def trim(data: pd.DataFrame) -> pd.DataFrame:
+
+    """
+    If starting or ending of any row contain black space ' ' this function will remove all those.
+    This feature is only applied for object types columns.
+
+    Returns
+        DataFrame
+    """
+    try:
+        for col in data.select_dtypes("object").columns:
+            # Check if column having string that starts or ends wwith blank space " "
+            if (data[col].str.startswith(" ").any() == True) or (
+                data[col].str.endswith(" ").any() == True
+            ):
+                data.loc[:, col] = data[col].astype("str").str.strip()
+
+        print("Trimming applied successfully")
+        return data
+    except:
+        print("Unable to apply trimming")
+        print(format_exc())
